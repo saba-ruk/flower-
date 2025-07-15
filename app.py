66ab3f0cdb2fc1fb906aa045eb6aaeb4cf0c18f2
saba-ruk <142679973+saba-ruk@users.noreply.github.com>
2025-07-16 02:51:53 +0530

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import keras
 from keras.models import load_model
 import streamlit as st 
 import tensorflow as tf
-from my_custom_module import MyCustomLayer  
 import numpy as np
 
 # Header
@@ -14,7 +13,7 @@ st.header('🌸 Flower Classification CNN Model')
 flower_names = ['daisy', 'dandelion','rose', 'sunflower', 'tulip']
 
 # Load trained model
-model = load_model("Flower_Recog_Model.h5", compile=False, custom_objects={"MyCustomLayer": MyCustomLayer})
+model = load_model("Flower_Recog_Model.h5")
 
 def classify_images(image_path):
     input_image = tf.keras.utils.load_img(image_path, target_size=(180,180))
